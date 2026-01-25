8ad9a81bad93855ce6af98ffdcd3097dbea5d45a
ayoadeyera <guitarsolomon@gmail.com>
2026-01-25 02:49:30 +0100

    pppp

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -3,7 +3,5 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 // 2. Last Modified Date
-// This captures the date the file was last saved/uploaded to the server
 let lastMod = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modification: ${lastMod}`;
-
